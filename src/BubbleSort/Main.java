33676e02355dd6f2353e9aa42e00504321c2e42d
BilalCoder <bilalkhankhurai@gmail.com>
2022-12-03 19:20:25 +0530

    completed the code for the Selection and bubble sort

---
+++ src/BubbleSort/Main.java
@@ -0,0 +1,30 @@
+package BubbleSort;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = {3, 5, 1, 10, 6, 0, 2, 1, 7, 4, 9, 8};
+        bubbleSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void bubbleSort(int[] array) {
+        int temp;
+        boolean isSorted;
+        for(int i = 0; i<array.length; i++){
+            isSorted = true;
+            for(int j = 1; j<array.length-i; j++) {
+                if(array[j-1] > array[j]) {
+                    temp = array[j-1];
+                    array[j-1] = array[j];
+                    array[j] = temp;
+                    isSorted = false;
+                }
+            }
+            if (isSorted)
+                return;
+        }
+    }
+}
---
+++ src/SelectionSort/Main.java
@@ -0,0 +1,26 @@
+package SelectionSort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {3, 5, 1, 10, 6, 0, 2, 1, 7, 4, 9, 8};
+        selectionSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void selectionSort(int[] array) {
+        int temp, minIndex;
+        for(int i=0; i<array.length; i++) {
+            minIndex = i;
+            for(int j=i; j<array.length; j++) {
+                if(array[minIndex] > array[j]) {
+                    minIndex = j;
+                }
+            }
+            temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+        }
+    }
+}
