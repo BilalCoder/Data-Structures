06b84b93e9fe71dd1f33f3011a43ae1f27528e8a
BilalCoder <bilalkhankhurai@gmail.com>
2022-12-07 00:01:51 +0530

    added the code for the binary tree

--- src/BinaryTree/Main.java
+++ src/BinaryTree/Main.java
@@ -1,4 +1,21 @@
 package BinaryTree;
 
 public class Main {
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        System.out.println(tree.isEmpty());
+        tree.insert(7);
+        System.out.println(tree.isEmpty());
+        tree.insert(4);
+        tree.insert(9);
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(8);
+        tree.insert(10);
+        System.out.println(tree.find(10));
+        System.out.println(tree.find(101));
+        System.out.println(tree.find(1));
+        System.out.println(tree.find(2));
+
+    }
 }
--- src/BinaryTree/Tree.java
+++ src/BinaryTree/Tree.java
@@ -1,4 +1,57 @@
 package BinaryTree;
 
 public class Tree {
+    private Node root;
+    private class Node {
+        private int value;
+        private Node left;
+        private Node right;
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public void insert(int value) {
+        Node node = new Node(value);
+        if(isEmpty()) {
+            root = node;
+            return;
+        }
+        else {
+            Node current = root;
+            while(true) {
+                if(value > current.value) {
+                    if (current.right == null) {
+                        current.right = node;
+                        break;
+                    }
+                    current = current.right;
+                }
+                else {
+                    if (current.left == null) {
+                        current.left = node;
+                        break;
+                    }
+                    current = current.left;
+                }
+            }
+        }
+    }
+
+    public boolean find(int value) {
+        Node current = root;
+        while(current != null) {
+            if(value < current.value)
+                current = current.left;
+            else if (value > current.value)
+                current = current.right;
+            else
+                return true;
+        }
+        return false;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
 }
